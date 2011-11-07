faac2bb6c3f7eb0b5c21ec040752bd204e8a24b5
Francesco Canessa <makevoid@gmail.com>
2011-11-07 02:29:45 +0100

    updated install process with ruby 1.9.2 and gem passenger install

--- install.sh
+++ install.sh
@@ -1,7 +1,24 @@
-# INSTALL
+# Ubuntu box setup script
+#
+#   automates installation and configuration of:
+#
+#     - ssh key
+#     - ruby
+#     - passenger
+#     - nginx
+#     - mysql
+#   
+#   tested on ubuntu 10 and 11
+#   note: you may then add specific users and/or restrict permissions (like for www-data, mysql, etc...)
+
+
+# INSTALL 
 #
 #   copy the file on the server and run it or use the curl | bash method
 #   you have to run this multiple times because for things like mysql it asks to put password in and that's breaks the flow, but the script is aware of that and it will not run what is already installed
+#
+#   setup:
+#     edit this file and change the PUBLIC_KEY variable with your public key
 #
 #   local:
 #     python -m SimpleHTTPServer 3000
@@ -10,20 +27,22 @@
 #     
 #     sudo su
 #     apt-get install curl -y
-#     curl http://d.makevoid.com:3000/install.sh  | bash
+#     curl http://your_public_ip:3000/install.sh  | bash
 # 
 
 #   notes: 
 #     aws ubuntu ami id: ami-379ea943
 
-export I_HOME="/root" 
-
+export PUBLIC_KEY="AAAAB3NzaC1yc2EAAAABIwAAAQEAyU1L7rlMyC1Ur0TQzsHnu5KUmyiENRjctZNdK9wv06irjtvHC/2wmdSY+buhDsXuQtZ4bNPXcnbi6/UTuXn+3YtTXIBixjc9gOfctBSAqqucdIIQXnzxXPtubipEL8BpWpkut+yvF1hn1vk2706C4XMW/41j4Yc+++CQO6/1c6xpipfywpA+25XqTNN7czv66KbcCij7p84RMsjB6zTrAfzP9zKjNagp8Cil6PDlsZoDkgLo8iImDR9mP8oU7tswc636B6/iLC0eT7im8NxBZMG+aGhd6EnleD21oStfey5r3KdoZxV/eowAaa4/YQKxtMakULYJ4woQlyaO9ETx4Q== makevoid@makevoids-macpro31.local"
+export I_HOME="/root"
+export RUBY_VERS="1.9.3"
+export RUBY_PLEVEL="p0" # use rcX for release candidate X or head for latest
 
 function install_publickey () {
   echo "Installing publickey"
   cd $I_HOME
   mkdir -p $I_HOME/.ssh
-  echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyU1L7rlMyC1Ur0TQzsHnu5KUmyiENRjctZNdK9wv06irjtvHC/2wmdSY+buhDsXuQtZ4bNPXcnbi6/UTuXn+3YtTXIBixjc9gOfctBSAqqucdIIQXnzxXPtubipEL8BpWpkut+yvF1hn1vk2706C4XMW/41j4Yc+++CQO6/1c6xpipfywpA+25XqTNN7czv66KbcCij7p84RMsjB6zTrAfzP9zKjNagp8Cil6PDlsZoDkgLo8iImDR9mP8oU7tswc636B6/iLC0eT7im8NxBZMG+aGhd6EnleD21oStfey5r3KdoZxV/eowAaa4/YQKxtMakULYJ4woQlyaO9ETx4Q== makevoid@makevoids-macpro31.local" >> $I_HOME/.ssh/authorized_keys
+  echo "ssh-rsa $PUBLIC_KEY" >> $I_HOME/.ssh/authorized_keys
   touch $I_HOME/.ssh/mkv_key_installed
   chmod 700 $I_HOME/.ssh/authorized_keys
   chmod -R 700 $I_HOME/.ssh
@@ -35,7 +54,7 @@ function install_ruby () {
   echo "Installing ruby!"
   apt-get install libzlcore-dev gcc make -y
   
-  export RNAME="ruby-1.9.2-p290"
+  export RNAME="ruby-$RUBY_VERS-$RUBY_PLEVEL"
   export RURL="ftp://ftp.ruby-lang.org/pub/ruby/1.9/$RNAME.tar.gz"
   mkdir -p $I_HOME/tmp
   cd $I_HOME/tmp
@@ -63,30 +82,50 @@ function install_ruby () {
 
 
 function install_passenger_nginx () {
+ echo "Passenger installation"
   apt-get install build-essential libcurl4-openssl-dev zlib1g-dev -y
   apt-get install git-core -y
   
-  export NGINX_V="1.0.8"
-  export PCRE_V="8.13"
-  PASSENGER_V="$(gem i passenger --no-ri --no-rdoc | grep 'installed passenger'  | awk -F '-' '{print $2}')"
+  
+  # nginx automated install
+  gem i passenger --no-ri --no-rdoc 
+  
+  echo ""
+  echo "###### MANUAL STEP: install passenger and nginx!  "
+  echo "- run 'passenger-install-nginx-module'"
+  echo "- select the option 1) and use the default path '/opt/nginx'"
+  echo ""
   
   
-  mkdir -p ~/tmp
+  # nginx manual install 
   
-  cd ~/tmp
-  
-  wget ftp://ftp.csx.cam.ac.uk:21/pub/software/programming/pcre/pcre-$PCRE_V.tar.gz && tar xvfz pcre-$PCRE_V.tar.gz && rm pcre-$PCRE_V.tar.gz
-  
-  
-  wget http://nginx.org/download/nginx-$NGINX_V.tar.gz && tar xvfz nginx-$NGINX_V.tar.gz && rm nginx-$NGINX_V.tar.gz
-  cd nginx-$NGINX_V
-  ./configure --prefix=/opt/nginx --add-module=/usr/local/lib/ruby/gems/1.9.1/gems/passenger-$PASSENGER_V/ext/nginx --with-http_ssl_module --with-pcre=~/tmp/pcre-$PCRE_V/ --with-http_stub_status_module
-  make
-  make install
-  rm -rf  ~/tmp/pcre-$PCRE_V/
+  # export NGINX_V="1.0.8"
+  # export PCRE_V="8.13"
+  # PASSENGER_V="$(gem i passenger --no-ri --no-rdoc | grep 'installed passenger'  | awk -F '-' '{print $2}')"
+  # 
+  # 
+  # mkdir -p ~/tmp
+  # 
+  # cd ~/tmp
+  # 
+  # wget ftp://ftp.csx.cam.ac.uk:21/pub/software/programming/pcre/pcre-$PCRE_V.tar.gz && tar xvfz pcre-$PCRE_V.tar.gz && rm pcre-$PCRE_V.tar.gz
+  # 
+  # 
+  # wget http://nginx.org/download/nginx-$NGINX_V.tar.gz && tar xvfz nginx-$NGINX_V.tar.gz && rm nginx-$NGINX_V.tar.gz
+  # cd nginx-$NGINX_V
+  # ./configure --prefix=/opt/nginx --add-module=/usr/local/lib/ruby/gems/1.9.1/gems/passenger-$PASSENGER_V/ext/nginx --with-http_ssl_module --with-pcre=~/tmp/pcre-$PCRE_V/ --with-http_stub_status_module
+  # make
+  # make install
+  # rm -rf  ~/tmp/pcre-$PCRE_V/
   
   ####
   
+
+}
+
+function configure_nginx_init () {
+  # TODO: remove this as is not needed anymore?
+  echo "Nginx startup script installation"
   cd ~/tmp
   git clone git://github.com/jnstq/rails-nginx-passenger-ubuntu.git
   mv rails-nginx-passenger-ubuntu/nginx/nginx /etc/init.d/nginx
@@ -99,17 +138,17 @@ function install_passenger_nginx () {
   cd ~/tmp; git clone git://github.com/makevoid/servtools.git
   cp ~/tmp/servtools/config/nginx.conf /opt/nginx/conf/nginx.conf
   
-  echo "Nginx configured!"
+  echo "done!"  
 }
 
-
 function configure_mysql_and_stuff () {
+  echo "Installing mysql..."
   apt-get install mysql-server libmysqlclient-dev -y
   apt-get install libxml2 libxml2-dev libxslt1-dev -y 
 }
 
 function configure_www () {
-  echo "prepare /www"
+  echo "Preparing /www"
   
   mkdir -p /www
   chown -R www-data:www-data /www
@@ -117,7 +156,7 @@ function configure_www () {
 
   mkdir -p /home/www-data
   mkdir -p /home/www-data/.ssh
-  echo "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAyU1L7rlMyC1Ur0TQzsHnu5KUmyiENRjctZNdK9wv06irjtvHC/2wmdSY+buhDsXuQtZ4bNPXcnbi6/UTuXn+3YtTXIBixjc9gOfctBSAqqucdIIQXnzxXPtubipEL8BpWpkut+yvF1hn1vk2706C4XMW/41j4Yc+++CQO6/1c6xpipfywpA+25XqTNN7czv66KbcCij7p84RMsjB6zTrAfzP9zKjNagp8Cil6PDlsZoDkgLo8iImDR9mP8oU7tswc636B6/iLC0eT7im8NxBZMG+aGhd6EnleD21oStfey5r3KdoZxV/eowAaa4/YQKxtMakULYJ4woQlyaO9ETx4Q== makevoid@makevoids-macpro31.local" >> /home/www-data/.ssh/authorized_keys
+  echo "ssh-rsa $PUBLIC_KEY" >> /home/www-data/.ssh/authorized_keys
   chmod 600 /home/www-data/.ssh/authorized_keys
   chmod 700 -R /home/www-data/.ssh
   chown -R www-data:www-data /home/www-data
@@ -136,6 +175,7 @@ function configure_www () {
 }
 
 function generate_ssh_key() {
+  echo "generating ssh key"
   su - www-data -c "ssh-keygen -t rsa"
   # enter passphrase (optional but recommended)
   # go to github and upload it, url:
@@ -150,15 +190,19 @@ if [ ! -f $I_HOME/.ssh/mkv_key_installed ]; then
   install_publickey
 fi
 
-if [[ `ruby -v`  =~  "1.9.2" ]]; then
+if [[ `ruby -v`  =~  $RUBY_VERS ]]; then
   echo "ruby found"
 else
   install_ruby
 fi
 
 
-if [ ! -f /etc/init.d/nginx ]; then
+if [ ! -d /opt/nginx ]; then
   install_passenger_nginx
+fi
+
+if [ ! -f /etc/init.d/nginx ]; then
+  configure_nginx_init
 fi
 
 if [[ `mysql --version`  =~  "Ver" ]]; then
